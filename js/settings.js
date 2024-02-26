218ff6f9aea8ba0c51ea21d78325c25210da9309
HannesPranni <akk51580@edu.redu.fi>
2024-02-26 17:06:01 +0200

    Localstorage news feed style test

--- js/settings.js
+++ js/settings.js
@@ -1,36 +1,41 @@
 // Funktio, joka tallentaa näkymäasetuksen localStorageen
 function saveViewPreference(view) {
-    localStorage.setItem("preferredView", view);
+  localStorage.setItem("preferredView", view);
 }
 
 // Funktio, joka tallentaa näkymäasetuksen localStorageen ja vaihtaa näkymän
 function switchView() {
-    const gridView = document.getElementById("gridView");
-    const listView = document.getElementById("listView");
-    const newsFeed = document.getElementById("news-feed");
+  const gridView = document.getElementById("gridView");
+  const listView = document.getElementById("listView");
+  const newsFeed = document.getElementById("news-feed");
 
-    if (gridView.checked) {
-        saveViewPreference("grid");
-        newsFeed.classList.remove("list");
-        newsFeed.classList.add("grid");
-    } else if (listView.checked) {
-        saveViewPreference("list");
-        newsFeed.classList.remove("grid");
-        newsFeed.classList.add("list");
-    }
+  if (gridView.checked) {
+    saveViewPreference("grid");
+    newsFeed.classList.remove("list");
+    newsFeed.classList.add("grid");
+  } else if (listView.checked) {
+    saveViewPreference("list");
+    newsFeed.classList.remove("grid");
+    newsFeed.classList.add("list");
+  }
 }
 
+document.addEventListener("DOMContentLoaded", function () {
+  const preferredView = localStorage.getItem("preferredView");
+  const newsFeed = document.getElementById("news-feed");
 
-document.addEventListener("DOMContentLoaded", function() {
-    // Haetaan tallennettu näkymäasetus localStoragesta
-    const preferredView = localStorage.getItem("preferredView");
-
-    // Jos asetus on tallennettu ja se on 'grid', valitaan 'grid' näkymä
-    if (preferredView === "grid") {
-        document.getElementById("gridView").checked = true;
-    } 
-    // Jos asetus on tallennettu ja se on 'list', valitaan 'list' näkymä
-    else if (preferredView === "list") {
-        document.getElementById("listView").checked = true;
-    }
+  // Jos asetus on tallennettu ja se on 'grid', valitaan 'grid' näkymä
+  if (preferredView === "grid") {
+    document.getElementById("gridView").checked = true;
+    newsFeed.classList.remove("list");
+    newsFeed.classList.add("grid");
+  }
+  // Jos asetus on tallennettu ja se on 'list', valitaan 'list' näkymä
+  else if (preferredView === "list") {
+    document.getElementById("listView").checked = true;
+    newsFeed.classList.remove("grid");
+    newsFeed.classList.add("list");
+  }
 });
+
+console.log(view);
